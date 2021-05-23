53aaa9a39e6c9a09c96ee144d6e971f570668712
Nur Ulfa Mauludina/19082010093 <79494782+NurUlfa23@users.noreply.github.com>
2021-05-23 19:21:46 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+// konfigurasi database
+$host = "localhost"; // Nama host
+$user = "root"; // Nama user
+$password =""; // Password
+$database = "db_siswa"; // Nama database
+// perintah php untuk akses ke database
+$koneksi = mysqli_connect($host, $user, $password, $database); // Variabel koneksi untuk menyambungkan ke database
+?>
